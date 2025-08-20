ec731132269aeb0b11a68c3700ff4b1732b01284
Kwesil <nwabikejr@gmail.com>
2025-08-20 03:41:45 +0100

    attributes, oop2.py

---
+++ oop2.py
@@ -0,0 +1,29 @@
+# attributes
+class Item:
+    pay_rate = 0.8  # The pay rate after 20% discount
+
+    def __init__(self, name: str, price: float, quantity: int):  # constructor
+        """This is the constructor method that initializes the instance variables."""
+
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
+        
+        # Assign to self object variables
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+    
+    def apply_discount(self):
+        """This method applies the discount to the price."""
+        self.price = self.price * self.pay_rate
+
+
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5) 
