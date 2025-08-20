ba9246ab57af2b99fe4b44850c5beffbb88e9b97
Kwesil <nwabikejr@gmail.com>
2025-08-20 02:55:32 +0100

    added validations to ensure no negative parameter is passed in

--- oop1.py
+++ oop1.py
@@ -1,12 +1,18 @@
 class Item:
-    def __init__(self, name, price, quantity):
-        print(f"An instance created: {name}")
+    def __init__(self, name: str, price: float, quantity: int): # constructor
+        """This is the constructor method that initializes the instance variables."""
+
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
+        
+        # Assign to self object variables
         self.name = name
         self.price = price
         self.quantity = quantity
 
-    def calculate_total_price(self, x, y):
-        return x *  y
+    def calculate_total_price(self):
+        return self.price *  self
 
 item1 = Item("Phone", 100, 5)
 """item1.name = 'Phone'
