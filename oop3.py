c39a34a4857096a37d7f67bc25066fbceaefc39c
Kwesil <nwabikejr@gmail.com>
2025-08-21 06:42:51 +0100

    Class and static methods

---
+++ oop3.py
@@ -0,0 +1,36 @@
+# Class and static methods
+# attributes
+class Item:
+    pay_rate = 0.8  # The pay rate after 20% discount
+    all = []  # This is a class variable that will hold all instances of Item
+
+    def __init__(self, name: str, price: float, quantity: int):  # constructor
+        """This is the constructor method that initializes the instance variables."""
+
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
+        
+        # Assign to self object variables
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        # Actions to execute
+        Item.all.append(self)
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+    
+    def apply_discount(self):
+        """This method applies the discount to the price."""
+        self.price = self.price * self.pay_rate
+
+    def instantiate_from_csv(self):
+
+
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+
+
+Item.instantiate_from_csv()
