daacaf54cb1f72c007337b19a49c26d8d2eced5f
Kwesil <nwabikejr@gmail.com>
2025-08-24 19:56:09 +0100

    setting up the workflow

--- calculator.py
+++ calculator.py
@@ -6,5 +6,35 @@
 
 class calc:
     def __init__(self):
-        return 0;
+        return 0
+    
+    def add(self, x, y):
+        self.x = x
+        self.y = y
+        return x + y
+    
+    def sub(self, x, y):
+        self.x = x
+        self.y = y
+        return x - y
+    
+    def mul(self, x, y):
+        self.x = x
+        self.y = y
+        return x * y
+    
+    def div(self, x, y):
+        self.x = x
+        self.y = y
+        return x / y
+    
+    def logic(self, operator):
+        if operator == "addition":
+            return add(x, y)
 
+        return{}
+
+    
+x = float(input("Enter your first digit: "))
+y = float(input("Enter your second digit: "))
+operator = input("Enter your operator (addition subtraction, muliplication, division): ")
