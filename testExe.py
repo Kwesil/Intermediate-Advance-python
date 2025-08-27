3ca8295dadcb4e6417849b5545a7de2f4997eb81
Kwesil <nwabikejr@gmail.com>
2025-08-27 23:35:23 +0100

    Adding workflow

---
+++ testExe.py
@@ -0,0 +1,13 @@
+#step process
+# create a function for each process
+
+
+sandwich_order = ["Falafel Sandwich", "Egg Salad Sandwich", "Caprese Sandwwich", "Tuna Sandwich"]
+finished_sandwiches = []
+
+for sandwich in sandwich_order:
+    print(f"I made your {sandwich}")
+    finished_sandwiches.append(sandwich)
+
+ 
+print(finished_sandwiches)
